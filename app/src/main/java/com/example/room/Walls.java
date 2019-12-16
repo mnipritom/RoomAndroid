f586bf913e22ca511315241b679d63a540f05c46
那思路(なしろ) <imdnazrul90@gmail.com>
2019-12-16 15:31:22 +0800

    Revert "Starting to implement a GridView in Walls Activity"
    This reverts commit 65e62470d1c8dcb8596e7400962f2f710dba30f9.

--- app/src/main/java/com/example/room/Walls.java
+++ app/src/main/java/com/example/room/Walls.java
@@ -3,20 +3,12 @@ package com.example.room;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.GridView;
 
 public class Walls extends AppCompatActivity {
-
-    private GridView wallsGridView;
-    private String[] wallsListData;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.walls);
-
-        wallsGridView = (GridView)findViewById(R.id.wallsList);
-        wallsListData = getResources().getStringArray(R.array.wallsListArray);
     }
 }
